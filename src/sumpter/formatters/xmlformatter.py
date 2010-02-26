95a9db9c249dd4d0afc60b9b4cd3aa2425d5b2e9
Dhananjay Nene <dhananjay.nene@gmail.com>
2010-02-26 15:46:28 +0530

    Added support for xml parsing

--- src/sumpter/formatters/xmlformatter.py
+++ src/sumpter/formatters/xmlformatter.py
@@ -13,11 +13,14 @@
 #    See the License for the specific language governing permissions and 
 #    limitations under the License.
 ############################################################################# 
-from sumpter import Segment, Config
+from sumpter import Segment, Config, Generic
 from xml.dom import minidom
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
 import cStringIO
 import logging
 import types
+import xml.sax
 
 log = logging.getLogger(__name__)      
 
@@ -71,3 +74,92 @@ class SimpleXmlComposer(Segment):
             else :
                 self.compose(doc,element,name,val)
 
+class Element(object):
+    def __init__(self,name,attrs, trim = True):
+        self.name = name
+        self.attrs = dict((str(name), attrs.getValue(name)) for name in attrs.getNames())
+        self.content = []
+        self.trim = trim
+    def append_content(self,content):
+        if self.trim : content = content.strip()
+        if len(content) > 0 : self.content.append(content)
+    def get_content(self):
+        if self.trim :
+            return " ".join(self.content)
+        else :
+            return "".join(self.content)
+    def to_generic(self):
+        return Generic(**self.attrs)
+    def add_attr(self,name,val):
+        self.attrs[str(name)] = val
+    def __repr__(self):
+        return 'Element[%s]' % self.name
+
+class ElementList(object):
+    def __init__(self,name,attrs, trim = True):
+        self.name = name
+        self.content = []
+        self.attrs = []
+        self.trim = trim
+    def append_content(self,content):
+        if self.trim : content = content.strip()
+        if len(content) > 0 : self.content.append(content)
+    def add_attr(self,name,val):
+        self.attrs.append((name,val))
+    def to_generic(self):
+        return tuple((name, val.to_generic if isinstance(val,(Element,ElementList)) else val) for name,val in self.attrs)
+        
+class XmlHandler(xml.sax.handler.ContentHandler):
+    def __init__(self,multitags):
+        ContentHandler.__init__(self)
+        self.current = None
+        self.stack = []
+        self.result = None
+        self.multitags = multitags
+                
+    def startElement(self, name, attrs) :
+        if self.current : self.stack.append(self.current)
+        if name in self.multitags :
+            self.current = ElementList(name,attrs)
+        else :
+            self.current = Element(name,attrs)
+        
+    def characters(self,content):
+        self.current.append_content(content)
+        
+    def endElement(self,name):
+        if self.current.name != name : raise Exception('Internal error')
+        child = self.current
+        if len(self.stack) > 0 :
+            self.current = self.stack.pop()
+        else :
+            self.current = None
+        
+        if self.current :
+            if len(child.attrs) > 0 :
+                self.current.add_attr(name,child.to_generic())
+            else :
+                self.current.add_attr(name,child.get_content())
+        else :
+            if len(child.attrs) > 0 :
+                self.result = child.to_generic()
+            else :
+                self.result = child.get_content()
+            
+
+        
+class SimpleXmlParser(Segment) :
+    def __init__(self,name,**kwargs):
+        super(SimpleXmlParser,self).__init__(name,**kwargs)
+        
+    def perform(self,ctx,val):
+        config = Config(self,ctx,val,'xml','multitags')
+        if config.multitags :
+            config.multitags = config.multitags.split(',')
+        parser = make_parser()
+        handler = XmlHandler(config.multitags)
+        parser.setContentHandler(handler)
+        parser.parse(cStringIO.StringIO(config.xml))
+        val['model'] = handler.result
+
+    
